dce895a9402826b8653a947541a9e0bc757d9c2d
Sujit-Hiwale <sujithiwale88@gmail.com>
2025-11-14 17:12:32 +0530

    Added routes

--- server/index.js
+++ server/index.js
@@ -63,8 +63,31 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
-  res.json({ message: "Login successful", data: req.body });
+app.post("/login", async(req, res) => {
+  const token = req.headers.authorization?.split("Bearer ")[1]
+
+  if (!token)
+    return res.status(401).json({ error: "Unauthorized - No token provided"});
+  try{
+    const decoded = await admin.auth().verifyIdToken(token);
+    const uid = decoded.uid;
+
+    const userDoc = await db.collection("users").doc(uid).get();
+
+    if(!userDoc.exists)
+      return res.status(404).json({ error: "User profile not found"});
+
+    const userData = userDoc.data();
+
+    res.json({
+      message: "Login Successful",
+      firebaseUid: uid,
+      profile: userData,
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(401).json({ error: "Invalid or expired token "});
+  }
 });
 
 const PORT = process.env.PORT || 5000;
